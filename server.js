5633968d5046ac11fbafc42848bfd8520ef0b69d
ankit00010 <ankitmish83@gmail.com>
2025-03-17 01:14:25 +0530

    feat:"updated the cors url"

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://aalogistic.vercel.app",
+    origin: "https://aalogistics.vercel.app/",
     methods: ["POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
