0b6890fea2e44c8e5935ba0d5eb50f679f159743
ankit00010 <ankitmish83@gmail.com>
2025-03-17 01:16:50 +0530

    feat:"Added backend url for the cors"

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://aalogistics.vercel.app/",
+    origin: "https://logistics-hub-server.vercel.app/",
     methods: ["POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
